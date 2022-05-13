abcc0100e3b1e92f8d57d669284ea81b05ce1534
user <ulanskii03@gmail.com>
2022-05-13 15:37:41 +0600

    ulan

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -7,8 +7,19 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = applicationContext.getBean("helloworld", HelloWorld.class);
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(bean1.getMessage());
+
+        System.out.println(bean == bean1);
+
+        Cat one = applicationContext.getBean("cat", Cat.class);
+        System.out.println(one);
+        Cat two = applicationContext.getBean("cat", Cat.class);
+        System.out.println(one == two);
+
+
     }
 }
---
+++ src/main/java/peaksoft/Cat.java
@@ -0,0 +1,28 @@
+package peaksoft;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class Cat {
+    private String name;
+
+    public Cat() {
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "name: " + name;
+    }
+}
